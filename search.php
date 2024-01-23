4e1c5587135f2bdec9f80c5efe007457e5a4ba8d
Prakhar <prakhar2003.agrawal@gmail.com>
2024-01-24 00:11:36 +0530

    Update search.php

---
+++ search.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_GET['keyword'])) {
+    $search_query = $_GET['keyword'];
+    // Implement your search logic using $search_query
+    // For example:
+    // Run a database query or custom search algorithm using $search_query
+    // Retrieve search results
+}
+?>
